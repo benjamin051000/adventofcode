38ef43e5b3e1f56542ba1debedc10af8cb015da4
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-02 10:21:26 -0500

    added timeit for average runtime

--- solutions/day2/day2_test.py
+++ solutions/day2/day2_test.py
@@ -2,13 +2,20 @@
 Day 2 tests
 Benjamin Wheeler
 """
+from timeit import Timer
 from day2.solution import part1, part2
 
 
 def test_part1():
     assert part1() == 564
+    t = Timer(part1)
+    runtime = t.timeit(number=1000)
+    print('\nAverage runtime', runtime)
 
 
 def test_part2():
     assert part2() == 325
+    t = Timer(part2)
+    runtime = t.timeit(number=1000)
+    print('\nAverage runtime', runtime)
 
