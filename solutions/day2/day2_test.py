3cfa51bcbf95077130625b8e445a384a140cf784
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-02 09:15:35 -0500

    added day 2

---
+++ solutions/day2/day2_test.py
@@ -0,0 +1,13 @@
+"""
+Day 2 tests
+Benjamin Wheeler
+"""
+from day2.solution import part1, part2
+
+def test_part1():
+    pass
+
+
+def test_part2():
+    pass
+
---
+++ solutions/day2/solution.py
@@ -0,0 +1,13 @@
+"""
+Day 2 initial solution
+Benjamin Wheeler
+"""
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
