624356198f297a96e9b8217483c811b7d5bc5ffb
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-03 23:49:44 -0500

    added test

---
+++ solutions/day3/day3_test.py
@@ -0,0 +1,13 @@
+"""
+Day 3 tests
+Benjamin Wheeler
+"""
+from day3.solution import part1, part2
+
+
+def test_part1():
+    assert part1() == 187
+
+
+def test_part2():
+    assert part2() == 4723283400
