38e4b05bfe5955a5a134f0951888419308443754
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-05 11:39:56 -0500

    added unit tests

---
+++ solutions/day5/day5_test.py
@@ -0,0 +1,22 @@
+"""
+Day 5 tests
+Benjamin Wheeler
+"""
+from day5.solution import *
+
+
+def test_get_row_col():
+    seats = ('FBFBBFFRLR', 'BFFFBBFRRR', 'FFFBBBFRRR', 'BBFFBBFRLL')
+    r_c = ((44, 5), (70, 7), (14, 7), (102, 4))
+
+    for seat, ans in zip(seats, r_c):
+        assert get_row_col(seat) == ans
+
+
+def test_part1():
+    assert part1() == 998
+
+
+def test_part2():
+    assert part2() == 676
+
