e53953782145e8dbb5ca3598d801a44b48791c26
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-07 14:44:31 -0500

    [Solved] both parts done with unit tests for recursive funcs

---
+++ solutions/day7/day7_test.py
@@ -0,0 +1,60 @@
+"""
+Day 7 tests
+Benjamin Wheeler
+"""
+from textwrap import dedent
+from day7.solution import part1, part2
+
+
+def test_part1_example_input():
+    test_input = dedent("""\
+    light red bags contain 1 bright white bag, 2 muted yellow bags.
+    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    bright white bags contain 1 shiny gold bag.
+    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    faded blue bags contain no other bags.
+    dotted black bags contain no other bags.
+    """)
+
+    assert part1(test_input) == 4
+
+
+def test_part2_example1_input():
+    test_input = dedent("""\
+    light red bags contain 1 bright white bag, 2 muted yellow bags.
+    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+    bright white bags contain 1 shiny gold bag.
+    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    faded blue bags contain no other bags.
+    dotted black bags contain no other bags.
+    """)
+
+    assert part2(test_input) == 32
+
+
+def test_part2_example2_input():
+    test_input = dedent("""\
+    shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags.
+    """)
+
+    assert part2(test_input) == 126
+
+
+def test_part1():
+    assert part1() == 248
+
+
+def test_part2():
+    assert part2() == 57281
---
+++ solutions/day7/solution.py
@@ -0,0 +1,95 @@
+"""
+Day 7 initial solution
+Benjamin Wheeler
+"""
+import re
+
+
+def parse_bags(rules: list = None) -> dict:
+    """ Parse the rules and return a dictionary
+    to lookup what each bag contains. """
+    output = {}
+
+    for rule in rules:
+        key, val = rule.split(' bags contain ')
+        val = {k: int(v) for v, k in re.findall(r'(\d+) (\D+) bag', val)}
+        output[key] = val
+
+    return output
+
+
+def find_bag(rules: dict, bag: str, key: str) -> bool:
+    """ Recursive function to find a given
+    bag key from bag start. If no key is given
+    (e.g. key = None), then return every empty bag. """
+    # Base: key is in this bag.
+    if key in rules[bag].keys():
+        return True
+    # Base: bag is empty
+    elif not rules[bag]:
+        return False
+    # Recursive: Search through each subsequent bag (DFS) for the key.
+    # (Iterates through each key in rules[bag])
+    found = False
+    for next_bag in rules[bag]:
+        found |= find_bag(rules, next_bag, key)
+
+    return found
+
+
+def tally_bags(rules: dict, bag: str) -> int:
+    """ Recursively returns the total number of bags a bag
+    contains, including the bag itself. """
+    # Base: bag is empty.
+    if not rules[bag]:
+        # Return the bag itself.
+        return 1
+
+    # Recursive: DFS through the rest of the bags.
+    total = 0
+    for b, num in rules[bag].items():
+        temp = tally_bags(rules, b)
+        total += temp * num
+
+    # Return the bag itself plus every bag inside it.
+    return total + 1
+
+
+def part1(rules: str = None) -> int:
+    if rules is None:
+        with open('day7.input', 'r') as f:
+            rules = f.read()
+
+    rules = rules.splitlines()
+    rules = parse_bags(rules)
+
+    total_bags = 0
+
+    for bag in rules:
+        if find_bag(rules, bag, 'shiny gold'):
+            total_bags += 1
+
+    return total_bags
+
+
+def part2(rules: str = None) -> int:
+    if rules is None:
+        with open('day7.input', 'r') as f:
+            rules = f.read()
+
+    rules = rules.splitlines()
+    rules = parse_bags(rules)
+
+    # Tally up every bag inside the shiny gold bag (but not including the shiny gold bag).
+    return tally_bags(rules, 'shiny gold') - 1
+
+
+if __name__ == '__main__':
+    print(f'Running day 7...')
+    answer = part1()
+    print('Part 1:', answer)
+
+    answer = part2()
+    print('Part 2:', answer)
+
+    print('Done.')
