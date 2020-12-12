f1cb30faa84531c2afca6fd4b261cc0766b57965
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-11 23:55:54 -0500

    Test solution for part 1

---
+++ solutions/day10/day10_test.py
@@ -0,0 +1,32 @@
+"""
+Day 10 tests
+Benjamin Wheeler
+"""
+from textwrap import dedent
+from day10.solution import part1, part2
+
+
+def test_part1():
+    pass
+
+
+def test_part1_example():
+    example = dedent("""\
+    16
+    10
+    15
+    5
+    1
+    11
+    7
+    19
+    6
+    12
+    4
+    """)
+    assert part1(example) == 7 * 5
+
+
+def test_part2():
+    pass
+
