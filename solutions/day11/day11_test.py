49e2d3bb957644d9f6b1727c46f631a51d883765
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-13 16:56:34 -0500

    [Part 2] solved

--- solutions/day11/day11_test.py
+++ solutions/day11/day11_test.py
@@ -3,7 +3,7 @@ Day 11 tests
 Benjamin Wheeler
 """
 from textwrap import dedent
-from day11.solution import part1, part2, simulate_once
+from day11.solution import part1, part2, get_first_visible
 
 
 def test_example1():
@@ -21,9 +21,54 @@ def test_example1():
     """)
     assert part1(example) == 37
 
+    assert part2(example) == 26
+
 
 def test_part1():
     assert part1() == 2361
+
+
+def test_first_visible():
+    """ Tests the get_first_visible function in part 2. """
+    examples = [
+        dedent("""\
+        .......#.
+        ...#.....
+        .#.......
+        .........
+        ..#L....#
+        ....#....
+        .........
+        #........
+        ...#.....
+        """),
+
+        dedent("""\
+        .............
+        .L.L.#.#.#.#.
+        .............
+        """),
+
+        dedent("""\
+        .##.##.
+        #.#.#.#
+        ##...##
+        ...L...
+        ##...##
+        #.#.#.#
+        .##.##.
+        """)
+    ]
+
+    test_chair = [(4, 3), (1, 1), (3, 3)]
+
+    answers = [8, 0, 0]  # Second entry sees one empty seat, but zero occupied seats.
+
+    for ex, ans, (r, c) in zip(examples, answers, test_chair):
+        # Construct 2D list
+        seats = [[char for char in row] for row in ex.splitlines()]
+
+        assert get_first_visible(seats, r, c) == ans
 
 
 def test_part2():
--- solutions/day11/solution.py
+++ solutions/day11/solution.py
@@ -29,7 +29,7 @@ def get_neighbors(seats, r, c):
     return neighbors
 
 
-def simulate_once(seats):
+def simulate_once_part1(seats):
     """ Simulates one iteration of seat changing.
      Returns the new seating arrangement and total occupied seats."""
 
@@ -65,7 +65,8 @@ def simulate_once(seats):
 
 
 def print_seats(seats, occ):
-    """ Prints the seats. """
+    """ Prints the seats.
+     For debugging purposes."""
     rows = [''.join(row) for row in seats]
     print()
     print('='*20)
@@ -93,7 +94,7 @@ def part1(lines: [str] = None) -> int:
     # Run the simulation until last == current
     while last != current:
         last = current
-        seats, current = simulate_once(seats)
+        seats, current = simulate_once_part1(seats)
         iterations += 1
 
     print(f'Solved simulation in {iterations} iterations.')
@@ -101,8 +102,89 @@ def part1(lines: [str] = None) -> int:
     return current
 
 
-def part2() -> int:
-    pass
+def get_first_visible(seats, row, col):
+    """ Returns the sum of the first visible seats
+    in each direction from a particular seat r, c."""
+    total_rows = len(seats)
+    total_cols = len(seats[0])
+
+    visible = 0
+
+    directions = [(v, h) for v, h in itertools.product(range(-1, 2), repeat=2)]
+    directions.remove((0, 0))
+
+    # For each direction, traverse until we hit a boundary or an occupied seat.
+    for direction in directions:
+        r = row + direction[0]
+        c = col + direction[1]
+
+        # Boundary check
+        while 0 <= r < total_rows and 0 <= c < total_cols:
+            if seats[r][c] == '#':
+                visible += 1
+                # All other neighbors in this direction are obstructed, so we're done.
+                break
+
+            elif seats[r][c] == 'L':
+                # This chair sees an empty seat. Move on
+                break
+
+            # Do it again one step further out.
+            r += direction[0]
+            c += direction[1]
+
+    return visible
+
+
+def simulate_once_part2(seats):
+    total_rows = len(seats)
+    total_cols = len(seats[0])
+    changes = []
+
+    for r, c in itertools.product(range(total_rows), range(total_cols)):
+        if seats[r][c] == '.':
+            continue
+
+        # Get sum of first visible neighbors in each direction.
+        neighbors = get_first_visible(seats, r, c)
+
+        # Next, see if this seat needs to change.
+        if seats[r][c] == 'L' and neighbors == 0:
+            changes.append((r, c, '#'))
+
+        elif seats[r][c] == '#' and neighbors >= 5:
+            changes.append((r, c, 'L'))
+
+    # Apply changes
+    for r, c, new in changes:
+        seats[r][c] = new
+
+    # Count total occupied
+    occupied = len([seat for row in seats for seat in row if seat == '#'])
+
+    return seats, occupied
+
+
+def part2(lines: [str] = None) -> int:
+    if not lines:
+        with open('day11.input', 'r') as f:
+            lines = f.read()
+
+    lines = lines.splitlines()
+    seats = [[char for char in row] for row in lines]
+
+    last = 0
+    current = -1
+    iterations = 0
+
+    while last != current:
+        last = current
+        seats, current = simulate_once_part2(seats)
+        iterations += 1
+
+    print(f'Solved simulation in {iterations} iterations.')
+
+    return current
 
 
 if __name__ == '__main__':
@@ -114,4 +196,3 @@ if __name__ == '__main__':
     print('Part 2:', answer)
 
     print('Done.')
-
