5054114bb547d174e5f42fa420177b9ed83c7fe2
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-13 17:59:12 -0500

    [Part 1] solved with tests

---
+++ solutions/day12/day12_test.py
@@ -0,0 +1,26 @@
+"""
+Day 12 tests
+Benjamin Wheeler
+"""
+from textwrap import dedent
+from day12.solution import part1, part2
+
+
+def test_example1():
+    example = dedent("""\
+    F10
+    N3
+    F7
+    R90
+    F11
+    """)
+    assert part1(example) == 25
+
+
+def test_part1():
+    assert part1() == 904
+
+
+def test_part2():
+    pass
+
---
+++ solutions/day12/solution.py
@@ -0,0 +1,61 @@
+"""
+Day 12 initial solution
+Benjamin Wheeler
+"""
+import turtle
+
+
+def part1(directions=None) -> int:
+    if not directions:
+        with open('day12.input', 'r') as f:
+            directions = f.read()
+
+    directions = directions.splitlines()
+
+    # Utilize a Turtle to follow the instructions!
+    s = turtle.Screen()
+    s.screensize(1200, 1600)
+
+    t = turtle.Turtle()
+
+    movements = {
+        'N': lambda dist: t.sety(t.ycor() + dist),
+        'S': lambda dist: t.sety(t.ycor() - dist),
+        'E': lambda dist: t.setx(t.xcor() + dist),
+        'W': lambda dist: t.setx(t.xcor() - dist),
+        'L': lambda deg: t.left(deg),
+        'R': lambda deg: t.right(deg),
+        'F': lambda dist: t.forward(dist)
+    }
+
+    for direction in directions:
+        command, *arg = direction
+        arg = int(''.join(arg))
+
+        # Resize the screen to keep the turtle onscreen
+        bbox = [0, 0, 0, 0]
+
+        # Run the command
+        movements[command](arg)
+
+    print('Turtle is done, you can now close the window.')
+    turtle.done()
+
+    # Return the distance.
+    return abs(int(t.xcor())) + abs(int(t.ycor()))
+
+
+def part2() -> int:
+    pass
+
+
+if __name__ == '__main__':
+    print(f'Running day 12...')
+    answer = part1()
+    print('Part 1:', answer)
+
+    answer = part2()
+    print('Part 2:', answer)
+
+    print('Done.')
+
