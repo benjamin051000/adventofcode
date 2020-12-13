fdcd04225aff6931578e217a40355703fd41092f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-13 15:39:42 -0500

    [Part 1] solved part 1

--- solutions/day11/day11_test.py
+++ solutions/day11/day11_test.py
@@ -2,11 +2,28 @@
 Day 11 tests
 Benjamin Wheeler
 """
-from day11.solution import part1, part2
+from textwrap import dedent
+from day11.solution import part1, part2, simulate_once
+
+
+def test_example1():
+    example = dedent("""\
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """)
+    assert part1(example) == 37
 
 
 def test_part1():
-    pass
+    assert part1() == 2361
 
 
 def test_part2():
--- solutions/day11/solution.py
+++ solutions/day11/solution.py
@@ -2,10 +2,103 @@
 Day 11 initial solution
 Benjamin Wheeler
 """
+import itertools
 
 
-def part1() -> int:
-    pass
+def get_neighbors(seats, r, c):
+    """ Returns number of neighbors around a particular seat. """
+    total_rows = len(seats)
+    total_cols = len(seats[0])
+
+    neighbors = 0
+
+    for v, h in itertools.product(range(-1, 2), repeat=2):
+        # Check for boundary errors
+        check_r = r + v
+        check_c = c + h
+        if not 0 <= check_r < total_rows or not 0 <= check_c < total_cols:
+            continue
+
+        # Skip own seat
+        elif check_r == r and check_c == c:
+            continue
+
+        elif seats[check_r][check_c] == '#':  # Occupied seat
+            neighbors += 1
+
+    return neighbors
+
+
+def simulate_once(seats):
+    """ Simulates one iteration of seat changing.
+     Returns the new seating arrangement and total occupied seats."""
+
+    # Use itertools.product to iterate through a 2D array a little simpler.
+    total_rows = len(seats)
+    total_cols = len(seats[0])
+
+    # Record the seats that will change from L to # or vice versa.
+    changes = []
+
+    for r, c in itertools.product(range(total_rows), range(total_cols)):
+        if seats[r][c] == '.':
+            continue
+
+        # Get sum of occupied seats around this one
+        neighbors = get_neighbors(seats, r, c)
+
+        # Next, see if this seat needs to change.
+        if seats[r][c] == 'L' and neighbors == 0:
+            changes.append((r, c, '#'))
+
+        elif seats[r][c] == '#' and neighbors >= 4:
+            changes.append((r, c, 'L'))
+
+    # Apply changes
+    for r, c, new in changes:
+        seats[r][c] = new
+
+    # Count total occupied
+    occupied = len([seat for row in seats for seat in row if seat == '#'])
+
+    return seats, occupied
+
+
+def print_seats(seats, occ):
+    """ Prints the seats. """
+    rows = [''.join(row) for row in seats]
+    print()
+    print('='*20)
+    print('\n'.join(rows))
+    print('Occupancy:', occ)
+    print('='*20)
+
+
+def part1(lines: [str] = None) -> int:
+    if not lines:
+        with open('day11.input', 'r') as f:
+            lines = f.read()
+
+    # Separate by line
+    lines = lines.splitlines()
+
+    # This is necessary because we can't reassign characters in a string.
+    seats = [[char for char in row] for row in lines]
+
+    last = 0
+    current = -1
+
+    iterations = 0
+
+    # Run the simulation until last == current
+    while last != current:
+        last = current
+        seats, current = simulate_once(seats)
+        iterations += 1
+
+    print(f'Solved simulation in {iterations} iterations.')
+
+    return current
 
 
 def part2() -> int:
