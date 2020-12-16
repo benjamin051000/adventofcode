cc7ec7f60f83afcd2d8e52708695c495386afa26
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-16 02:17:33 -0500

    added some documentation

--- solutions/day13/solution.py
+++ solutions/day13/solution.py
@@ -12,8 +12,11 @@ def part1() -> int:
     earliest = int(earliest)
     routes = [int(r) for r in routes.split(',') if r != 'x']
 
+    # earliest % route gives the amount of time we missed the route by.
+    # Modulo operator is useful because the routes are periodic (reset to 0).
     next_buses = [route - earliest % route for route in routes]
 
+    # Pick the one coming up the soonest.
     next_bus = min(next_buses)
 
     return next_bus * routes[next_buses.index(next_bus)]
