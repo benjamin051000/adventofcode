43ab6d7418c9bb5193361ad62c609313c347e3ac
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2020-12-16 23:05:11 -0500

    [WIP] part 2 not working

--- solutions/day13/day13_test.py
+++ solutions/day13/day13_test.py
@@ -2,13 +2,32 @@
 Day 13 tests
 Benjamin Wheeler
 """
-from day13.solution import part1, part2
+from day13.solution import part1, part2, chinese
 
 
 def test_part1():
     assert part1() == 3385
 
 
-def test_part2():
-    pass
+def test_chinese():
+    b = [3, 1, 6]
+    n = [5, 7, 8]
+    assert chinese(remainders=b, modulae=n) == 78
 
+    b = [2, 3, 2]
+    n = [3, 5, 7]
+    assert chinese(remainders=b, modulae=n) == 23
+
+
+def test_part2():
+    tests = {
+        '7,13,x,x,59,x,31,19': 1068781,
+        '17,x,13,19': 3417,
+        '67,7,59,61': 754018,
+        '67,x,7,59,61': 779210,
+        '67,7,x,59,61': 1261476,
+        '1789,37,47,1889': 1202161486
+    }
+
+    for i, o in tests.items():
+        assert part2(i) == o
--- solutions/day13/solution.py
+++ solutions/day13/solution.py
@@ -2,11 +2,15 @@
 Day 13 initial solution
 Benjamin Wheeler
 """
+from functools import reduce
+import operator
 
 
-def part1() -> int:
-    with open('day13.input', 'r') as f:
-        earliest, routes = f.read().splitlines()
+def part1(lines=None) -> int:
+    if not lines:
+        with open('day13.input', 'r') as f:
+            lines = f.read()
+    earliest, routes = lines.splitlines()
 
     # Format input
     earliest = int(earliest)
@@ -22,8 +26,40 @@ def part1() -> int:
     return next_bus * routes[next_buses.index(next_bus)]
 
 
-def part2() -> int:
-    pass
+def chinese(*, remainders, modulae):
+    """ Performs the chinese remainder theorem. """
+
+    total = 0
+    product = reduce(operator.mul, modulae)
+
+    print('N =', product)
+
+    for remainder, mod in zip(remainders, modulae):
+        ni = product // mod
+
+        total += remainder * ni * pow(ni, -1, mod)  # modular multiplicative inverse (requires py3.8+)
+
+    return total % product
+
+
+def part2(routes=None) -> int:
+    if not routes:
+        with open('day13.input', 'r') as f:
+            _, routes = f.read().splitlines()
+
+    # Preserve the don't cares ('x') because index is important.
+    routes = [int(r) if r != 'x' else 'x' for r in routes.split(',')]
+
+    # Each remainder comes from the location in the base array.
+    b = [idx for idx, r in enumerate(routes) if r != 'x']
+
+    # Each modulae is the bus route time.
+    n = [r for r in routes if r != 'x']
+
+    # b[0] = routes[0]
+
+    # Find a number x such that x % answer[n] == n where n is an index 0 <= n < len(answer).
+    return chinese(remainders=b, modulae=n)
 
 
 if __name__ == '__main__':
