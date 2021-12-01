05a1bff619ff7cb4061f3241480a87c53cea87d8
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-12-01 01:14:26 -0500

    day 1 done! 🎄

--- 2021/day01/solution.py
+++ 2021/day01/solution.py
@@ -1,15 +1,47 @@
 """
-Day 1 initial solution
+Day 1 solution
 Benjamin Wheeler
 """
 
 
-def part1() -> int:
-    pass
+def part1(lines=None) -> int:
+    if not lines:
+        with open('day01.input') as f:
+            lines = f.read()
+    
+    numbers = [int(line) for line in lines.splitlines()]
+    
+    prev_number = numbers[0]
+    count = 0
+
+    for curr_number in numbers[1:]:
+        if curr_number > prev_number:
+            count += 1
+        
+        prev_number = curr_number
+    
+    return count
 
 
-def part2() -> int:
-    pass
+def part2(lines=None) -> int:
+    if not lines:
+        with open('day01.input') as f:
+            lines = f.read()
+    
+    numbers = [int(line) for line in lines.splitlines()]
+
+    prev_window = numbers[0:3]
+    count = 0
+
+    for idx, num in enumerate(numbers[1:]):
+        curr_window = numbers[idx:idx+3]
+        
+        if sum(curr_window) > sum(prev_window):
+            count += 1
+
+        prev_window = curr_window
+    
+    return count
 
 
 if __name__ == '__main__':
