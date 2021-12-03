63671e477a89bf83f2e5281bac0db7c4cfc7ecdd
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-12-02 23:58:53 -0500

    day2 initial setup

---
+++ 2021/day02/day2_test.py
@@ -0,0 +1,14 @@
+"""
+Day 2 tests
+Benjamin Wheeler
+"""
+from day02.solution import part1, part2
+
+
+def test_part1():
+    pass
+
+
+def test_part2():
+    pass
+
---
+++ 2021/day02/solution.py
@@ -0,0 +1,24 @@
+"""
+Day 2 initial solution
+Benjamin Wheeler
+"""
+
+
+def part1() -> int:
+    pass
+
+
+def part2() -> int:
+    pass
+
+
+if __name__ == '__main__':
+    print(f'Running day 2...')
+    answer = part1()
+    print('Part 1:', answer)
+
+    answer = part2()
+    print('Part 2:', answer)
+
+    print('Done.')
+
