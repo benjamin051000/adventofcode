39d30b7829c70f1190a616e897153fe5671b0d16
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2021-12-27 11:23:37 -0600

    part 1 done

--- 2021/day04/solution.py
+++ 2021/day04/solution.py
@@ -3,10 +3,55 @@ Advent of Code 2021
 Day 4 solution
 Author: Benjamin Wheeler
 """
+import itertools
+import numpy as np
 
 
 def part1(text: str) -> int:
-    lines = text.splitlines()
+    # Split text by empty lines
+    lines = text.split('\n\n')
+
+    # First line is the numbers
+    draws = map(int, lines.pop(0).split(','))
+
+    # The rest are the boards
+    boards = []
+    for board_text in lines:
+        boards.append(
+            [
+                [int(d) for d in l.split(' ') if d] 
+                for l in board_text.splitlines()
+            ]
+        )
+
+    boards = np.array(boards)
+    
+    # Create a 
+    scores = np.full(boards.shape, False)
+
+    # Find the board that wins first.
+    for draw in draws:
+        for board, score in zip(boards, scores):
+
+            # Check each board, scratch it off.
+            row, col = np.where(board == draw)
+        
+            if len(row) and len(col):
+                # Update score
+                row = row[0]  # Unpack row to get value
+                col = col[0]
+                score[row, col] = True
+
+            # Check for winners (rows and columns)
+            row_winners = list(map(all, score))
+            col_winners = list(map(all, score.transpose()))
+            if any(row_winners) or any(col_winners):
+                # This is a winner!
+                # Sum up unmarked ones.
+                final_arr = np.where(score == True, 0, board)  # 0 for marked
+                total = np.sum(final_arr)
+                return total * draw  # Winning draw
+
     return 0
 
 
@@ -17,7 +62,7 @@ def part2(text: str) -> int:
 def main():
     print(f'Running day 4...')
 
-    with open('day02.input') as f:
+    with open('day04.input') as f:
         text = f.read()
 
     answer = part1(text)
