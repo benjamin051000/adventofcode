79573102d9c34a19c0d54257b50c03b5e4cebadb
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2021-12-29 15:33:25 -0600

    day 5 input

---
+++ 2021/day05/day5_test.py
@@ -0,0 +1,19 @@
+"""
+Advent of Code 2021
+Day 5 tests
+Author: Benjamin Wheeler
+"""
+from solution import part1, part2
+from textwrap import dedent
+
+text = dedent("""\
+
+    """)
+
+def test_part1():
+    assert part1(text) == 0
+
+
+def test_part2():
+    assert part2(text) == 0
+
