5720ccd8db76cd8b9b9dfda26f75bab3968db429
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-11-19 01:48:08 -0500

    day4 MD5 hash setup

---
+++ 2015/day04/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // No input file, just the following string
+    let contents = String::from("yzbqklnj");
+
+    println!("{contents}");
+}
+
+#[test]
+fn example1() {
+    let contents = String::from("abcdef");
+}
+
+#[test]
+fn example2() {
+    let contents = String::from("pqrstuv");
+}
