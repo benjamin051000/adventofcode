721ff56cec5295de1fc714e728584f3409ac6053
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-12-10 01:01:46 -0500

    Day 3 done!

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -2,11 +2,6 @@ use std::collections::HashSet;
 #[cfg(test)]
 use std::collections::HashMap;
 
-fn main() {
-    let contents = std::fs::read_to_string("day03_input.txt").expect("Read file successfully");
-
-    println!("Part 1: {}", part1(contents));
-}
 
 fn split_in_half(line: &str) -> [&str; 2] {
     let halfway = line.len() / 2;
@@ -19,31 +14,104 @@ fn split_in_half(line: &str) -> [&str; 2] {
     [left, right]
 }
 
-fn part1(contents: String) -> i32 {
+
+fn get_common<'a>(l_letters: &'a str, r_letters: &'a str) -> char {
     let mut l_rucksack: HashSet<char> = HashSet::new();
     let mut r_rucksack: HashSet<char> = HashSet::new();
 
-    for line in contents.lines() {
-        // Get both halves
-        let [left_stuff, right_stuff] = split_in_half(line);
-
-
-        for c in left_stuff.chars() {
-            if !l_rucksack.insert(c) {
-
-            }
-        }
-
-        for c in right_stuff.chars() {
-            if !r_rucksack.insert(c) {
-
-            }
-        }
-
-        let common: HashSet<_> = l_rucksack.intersection(&r_rucksack).collect();
-        dbg!(common);
+    for c in l_letters.chars() {
+        l_rucksack.insert(c);
     }
-    0
+    for c in r_letters.chars() {
+        r_rucksack.insert(c);
+    }
+
+    let s: HashSet<_> = l_rucksack.intersection(&r_rucksack).collect();
+    if let Some(c) = s.iter().next().cloned() {
+        *c
+    }
+    else {
+        unreachable!();
+    }
+}
+
+fn get_three<'a>(a: &'a str, b: &'a str, c: &'a str) -> char {
+    let x: HashSet<_> = a.chars().collect();
+    let y: HashSet<_> = b.chars().collect();
+    let z: HashSet<_> = c.chars().collect();
+
+    let first_two = x.intersection(&y).copied().collect::<HashSet<char>>();
+
+    let all_three = first_two.intersection(&z).collect::<HashSet<_>>();
+
+    if let Some(c) = all_three.iter().next().cloned() {
+        *c
+    }
+    else {
+        unreachable!();
+    }
+}
+
+
+fn get_priority(c: char) -> u32 {
+    if c.is_uppercase() {
+        c as u32 - 'A' as u32 + 27
+    }
+    else {
+        c as u32 - 'a' as u32 + 1
+    }
+}
+
+
+
+fn part1(contents: &String) -> u32 {
+    let mut total = 0;
+    for line in contents.lines() {
+        let [l_half, r_half] = split_in_half(line);
+        let common = get_common(l_half, r_half);
+        total += get_priority(common);
+
+    }
+
+    total
+}
+
+
+fn part2(contents: &String) -> u32 {
+    let mut total = 0;
+
+    // Separate lines into groups of 3
+    let mut v = [String::new(), String::new(), String::new()];
+
+    let mut i = 0;
+    for line in contents.lines() {
+        if i >= 3 { i = 0; } // Clamp i from 0-2
+
+        v[i] = String::from(line);
+
+        // Every third go around...
+        if i == 2 {
+            assert_eq!(v.len(), 3);
+
+            let [a, b, c] = &v;
+
+            let common = get_three(a, b, c);
+
+            total += get_priority(common);
+        }
+
+        i += 1;
+    }
+
+    total 
+}
+
+
+fn main() {
+    let contents = std::fs::read_to_string("day03_input.txt").expect("Read file successfully");
+
+    println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
 }
 
 
@@ -63,19 +131,60 @@ fn test_split_in_half() {
     }
 }
 
+
 #[test]
 fn test_intersection() {
     let tests = HashMap::from([
-        (["vJrwpWtwJgWr", "hcsFMMfFFhFp"], "p"),
-        (["jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"], "L"),
-        (["PmmdzqPrV", "vPwwTWBwg"], "P")
+        (["vJrwpWtwJgWr", "hcsFMMfFFhFp"], 'p'),
+        (["jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"], 'L'),
+        (["PmmdzqPrV", "vPwwTWBwg"], 'P')
     ]);
+
+    for ([l, r], correct) in tests {
+        assert_eq!(get_common(l, r), correct);
+    }
 }
 
-// let contents = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
-//                 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
-//                 PmmdzqPrVvPwwTWBwg\n\
-//                 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
-//                 ttgJtRGJQctTZtZT\n\
-//                 CrZsJsPPZsGzwwsLwLmpwMDw";
-//
+
+#[test]
+fn test_get_priority() {
+    let tests = HashMap::from([
+        ('a', 1),
+        ('b', 2),
+        ('z', 26),
+        ('A', 27),
+        ('B', 28),
+        ('Z', 52)
+    ]);
+
+    for (c, p) in tests {
+        assert_eq!(get_priority(c), p);
+    }
+}
+
+#[test]
+fn test_example1() {
+
+    let contents = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+                    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+                    PmmdzqPrVvPwwTWBwg\n\
+                    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+                    ttgJtRGJQctTZtZT\n\
+                    CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    assert_eq!(part1(&String::from(contents)), 157);
+}
+
+
+#[test]
+fn test_example2() {
+
+    let contents = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+                    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+                    PmmdzqPrVvPwwTWBwg\n\
+                    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+                    ttgJtRGJQctTZtZT\n\
+                    CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    assert_eq!(part2(&String::from(contents)), 70);
+}
