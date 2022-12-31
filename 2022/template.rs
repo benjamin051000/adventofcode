491b8e5cc39118734e78e89808b6b3680b39947e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-12-31 16:55:14 -0500

    updated template

--- 2022/template.rs
+++ 2022/template.rs
@@ -1,13 +1,13 @@
 fn part1(contents: &String) -> i32 {
-    0
+    todo!();
 }
 
 fn part2(contents: &String) -> i32 {
-    0
+    todo!();
 }
 
 fn main() {
-    let contents = std::fs::read_to_string("day04_input.txt").expect("Read file successfully");
+    let contents = std::fs::read_to_string("day00_input.txt").expect("Read file successfully");
     println!("Part 1: {}", part1(&contents));
     println!("Part 2: {}", part2(&contents));
 }
