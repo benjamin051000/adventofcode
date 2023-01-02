8cdf7710c4a3811b03ec7f45a26993b50c7ba70a
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-01-02 14:42:44 -0500

    day 9 init

---
+++ 2022/day09/src/main.rs
@@ -0,0 +1,24 @@
+fn part1(contents: &String) -> i32 {
+    todo!();
+}
+
+fn part2(contents: &String) -> i32 {
+    todo!();
+}
+
+fn main() {
+    let contents = std::fs::read_to_string("day00_input.txt").expect("Read file successfully");
+    println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
+}
+
+#[test]
+fn example1() {
+
+}
+
+#[test]
+fn example2() {
+
+}
+
