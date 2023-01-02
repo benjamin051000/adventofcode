d649df5ddb5d1b82a8d49aa9ad12c265b64c835e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-01-01 23:04:26 -0500

    done with day 8

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -1,5 +1,10 @@
-fn make_forest(text: &String) -> Vec<Vec<u32>> {
-    let mut forest: Vec<Vec<u32>> = text
+use std::collections::HashSet;
+use itertools::Either;
+
+type Forest = Vec<Vec<u32>>;
+
+fn make_forest(text: &String) -> Forest {
+    let mut forest: Forest = text
         .split("\n")
         .map(|line| 
             line
@@ -10,6 +15,8 @@ fn make_forest(text: &String) -> Vec<Vec<u32>> {
         .collect();
 
     // Remove trailing [] if there is one
+    // which would have come from a trailing "\n" 
+    // so it's okay to remove.
     if forest.last().unwrap().len() == 0 { forest.pop(); }
 
     forest
@@ -18,16 +25,33 @@ fn make_forest(text: &String) -> Vec<Vec<u32>> {
 
 // A tree is visible if it is taller than all trees before it (in a particular direction).
 // Return number of visible trees in this row.
-fn find_visible(row: Vec<u32>) -> Vec<bool> {
-    let mut max: i32 = -1;
-    let mut visibles = vec![false; row.len()];
+fn find_visibles(
+    forest: &Forest, 
+    is_transpose: bool,
+    rows: &Either<impl Iterator<Item = usize> + Clone, impl Iterator<Item = usize> + Clone>, 
+    cols: &Either<impl Iterator<Item = usize> + Clone, impl Iterator<Item = usize> + Clone>) 
+-> HashSet<(usize, usize)> {
+    // let mut visibles = vec![false; row.len()];
+    let mut visibles = HashSet::new();
 
-    for (idx, tree) in row.iter().enumerate() {
-        if *tree as i32 > max {
-            max = *tree as i32;
-            visibles[idx] = true;
+    for r in rows.clone() {
+        let mut max: i32 = -1;
+
+        for c in cols.clone() {
+            let tree = forest[r][c];
+
+
+            if tree as i32 > max {
+                max = tree as i32;
+                if is_transpose {
+                    visibles.insert((c, r)); // Un-flip it
+                }
+                else {
+                    visibles.insert((r, c));
+                }
+
+            }
         }
-    
     }
 
     visibles
@@ -49,100 +73,110 @@ fn transpose<T: Clone>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
 }
 
 
-/// OR together two 2D vectors.
-fn or(a: Vec<Vec<bool>>, b: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    assert_eq!(a.len(), b.len());
-    assert_eq!(a[0].len(), b[0].len());
+fn create_range(lo: usize, hi: usize) 
+-> Either<impl Iterator<Item = usize> + Clone, impl Iterator<Item = usize> + Clone> {
+    assert_ne!(lo, hi);
+    let rev = lo > hi;
 
-    let mut result: Vec<Vec<bool>> = vec![vec![false; a[0].len()]; a.len()];
+    if !rev {
+        Either::Left(lo..hi)
+    } else {
+        Either::Right((hi..lo).rev())
+    }
+}
 
-    for r in 0..a.len() {
-        for c in 0..a[0].len() {
-            result[r][c] = a[r][c] || b[r][c];
+fn part1(contents: &String) -> u32 {
+    let forest = make_forest(contents);
+
+    let row_normal = create_range(0, forest.len());
+    let col_normal = create_range(0, forest[0].len());
+
+    let col_rev = create_range(forest[0].len(), 0);
+
+    // horizontal views
+    let vis_from_left = find_visibles(&forest, false, &row_normal, &col_normal);
+    let vis_from_right = find_visibles(&forest, false, &row_normal, &col_rev);
+
+    // For vertical views, look at the forest's transpose.
+    let forest_t = transpose(forest);
+
+    let vis_from_top = find_visibles(&forest_t, true, &row_normal, &col_normal);
+    let vis_from_bot = find_visibles(&forest_t, true, &row_normal, &col_rev);
+
+    // Get the union of each one (to avoid duplicates)
+    let total = &(&(&vis_from_left | &vis_from_right) | &vis_from_top) | &vis_from_bot;
+
+    // Part 1 is the length of these
+    total.len() as u32
+}
+
+
+fn get_scenery_score(forest: &Forest, (this_r, this_c): (usize, usize)) -> u32 {
+
+    let rows = forest.len();
+    let cols = forest[0].len();
+
+    let this_tree = forest[this_r][this_c];
+
+    let mut west_score = 0;
+    for c in (0..this_c).rev() {
+        west_score += 1;
+
+        let tree = forest[this_r][c];
+        if tree >= this_tree {
+            break;
         }
     }
 
-    result
+    let mut south_score = 0;
+    for r in this_r+1..rows {
+        south_score += 1;
+
+        let tree = forest[r][this_c];
+        if tree >= this_tree {
+            break;
+        }
+    }
+
+    let mut north_score = 0;
+    for r in (0..this_r).rev() {
+        north_score += 1;
+
+        let tree = forest[r][this_c];
+        if tree >= this_tree {
+            break;
+        }
+    }
+
+    let mut east_score = 0;
+    for c in this_c+1..cols {
+        east_score += 1;
+
+        let tree = forest[this_r][c];
+        if tree >= this_tree {
+            break;
+        }
+    }
+
+    west_score * south_score * north_score * east_score
 }
 
 
-fn part1(contents: &String) -> u32 {
-    let mut forest = make_forest(contents);
+fn part2(_contents: &String) -> u32 {
+    let forest = make_forest(_contents);
 
-    // --------------------------
-    // left
-    // --------------------------
-    let mut vis_from_left = Vec::<Vec<bool>>::new();
+    let mut max = 0;
 
-    for row in &forest {
-        vis_from_left.push(find_visible(row.to_vec()));
+    for r in 0..forest.len() {
+        for c in 0..forest[0].len() {
+            let score = get_scenery_score(&forest, (r, c));
+            if score > max {
+                max = score;
+            }
+        }
     }
 
-    // --------------------------
-    // right
-    // --------------------------
-    let mut vis_from_right = Vec::<Vec<bool>>::new();
-
-    for row in &mut forest {
-        row.reverse();
-        vis_from_right.push(find_visible(row.to_vec()));
-    }
-    // Un-reverse right visible array
-    for row in &mut vis_from_right {
-        row.reverse();
-    }
-
-    // --------------------------
-    // top
-    // --------------------------
-    // BUG this isn't working
-    let mut forest_t = transpose(forest);
-
-    let mut vis_from_top = Vec::<Vec<bool>>::new();
-
-    for row in &forest_t {
-        vis_from_top.push(find_visible(row.to_vec()));
-    }
-    // Un-transpose vis 
-    vis_from_top = transpose(vis_from_top);
-
-    // --------------------------
-    // bottom
-    // --------------------------
-    let mut vis_from_bot = Vec::<Vec<bool>>::new();
-
-    for row in &mut forest_t {
-        row.reverse();
-        vis_from_bot.push(find_visible(row.to_vec()));
-    }
-    // Un-transpose and un-reverse vis_from_bot
-    vis_from_bot = transpose(vis_from_bot);
-    for row in &mut vis_from_bot {
-        row.reverse();
-    }
-
-    // --------------------------
-    // Calculate total
-    // --------------------------
-    let combined = or(vis_from_left, or(vis_from_right, or(vis_from_top, vis_from_bot)));
-    
-    combined
-        .iter()
-        .map(|row| 
-            row
-                .iter()
-                // Just get the true values
-                .filter(|x| 
-                    **x
-                )
-                .count() as u32
-        )
-        .sum()
-}
-
-
-fn part2(contents: &String) -> i32 {
-    todo!();
+    max
 }
 
 
@@ -159,13 +193,32 @@ fn example1() {
                     25512\n\
                     65332\n\
                     33549\n\
-                    35390".to_string();
+                    35390\n".to_string();
 
     assert_eq!(part1(&contents), 21);
 }
 
 #[test]
 fn example2() {
+    let contents = "30373\n\
+                    25512\n\
+                    65332\n\
+                    33549\n\
+                    35390\n".to_string();
 
+    assert_eq!(part2(&contents), 8);
 }
 
+#[test]
+fn test_scene_scores() {
+    let contents = "30373\n\
+                    25512\n\
+                    65332\n\
+                    33549\n\
+                    35390\n".to_string();
+
+    let forest = make_forest(&contents);
+
+    assert_eq!(get_scenery_score(&forest, (1, 2)), 4);
+    assert_eq!(get_scenery_score(&forest, (3, 2)), 8);
+}
