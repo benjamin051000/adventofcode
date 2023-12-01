a364573607f60d2fcc85c20cddfe7eb9a07ad9fd
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-11-30 23:38:49 -0600

    part 1 working and test passing

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -3,11 +3,17 @@ Advent of Code 2023
 Day 1 solution
 Author: Benjamin Wheeler
 """
-
+import re
 
 def part1(text: str) -> int:
+    total = 0
     lines = text.splitlines()
-    return 0
+    for line in lines:
+        digits = re.findall(r"\d", line)
+        first = digits[0]
+        last = digits[-1]
+        total += int(first + last)
+    return total
 
 
 def part2(text: str) -> int:
