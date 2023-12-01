7ff79b86bbdf014574970766f93d2b06c73c0ebf
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-12-01 00:38:02 -0600

    done with part2. TODO clean up

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -3,11 +3,12 @@ Advent of Code 2023
 Day 1 solution
 Author: Benjamin Wheeler
 """
+from typing import Tuple
 import re
 
 def part1(text: str) -> int:
-    total = 0
     lines = text.splitlines()
+    total = 0
     for line in lines:
         digits = re.findall(r"\d", line)
         first = digits[0]
@@ -15,10 +16,25 @@ def part1(text: str) -> int:
         total += int(first + last)
     return total
 
+def get_first_and_last_numbers(line: str) -> Tuple[str, str]:
+    terms = [r"\d", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    digits = re.findall(f"(?=({'|'.join(terms)}))", line)
+    first = digits[0]
+    last = digits[-1]
+    if first in terms:
+        first = str(terms.index(first))
+    if last in terms:
+        last = str(terms.index(last))
+    return first, last
 
 def part2(text: str) -> int:
     lines = text.splitlines()
-    return 0
+    total = 0
+    for line in lines:
+        first, last = get_first_and_last_numbers(line)
+        total += int(first + last)
+    return total
+
 
 def main():
     print(f'Running day 1...')
--- 2023/day01/day01_test.py
+++ 2023/day01/day01_test.py
@@ -6,17 +6,38 @@ Author: Benjamin Wheeler
 from day01 import part1, part2
 from textwrap import dedent
 
-text = dedent("""\
-    1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet
-    """)
 
-def test_part1():
+def test_part1_example():
+    text = dedent("""\
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+        """)
     assert part1(text) == 142
 
 
-def test_part2():
-    assert part2(text) == 0
+def test_part2_example():
+    text = dedent("""\
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        """)
+    assert part2(text) == 281
 
+##############################
+# WARNING: SPOILERS AHEAD!!!
+##############################
+def test_part1():
+    with open('day01_input.txt') as f:
+        text = f.read()
+    assert part1(text) == 53080
+
+def test_part2():
+    with open('day01_input.txt') as f:
+        text = f.read()
+    assert part2(text) == 53268
